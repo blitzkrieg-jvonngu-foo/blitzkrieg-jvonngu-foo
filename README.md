ccec5900a3fda80d2a8eaa2f54b54679f1eb0ac7
blitzkrieg-jvonngu-foo <blitzkrieg.jvonngu.foo@gmail.com>
2022-12-23 19:51:16 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+    <p align=center>
+        <i>"Where there is matter, there is geometry."<br>
+            --Johannes Kepler--</i>
+    </p>
---
+++ au.py
@@ -0,0 +1,47 @@
+import random as rd
+
+GREAT_LIST_PATH = './src/great_list.txt'
+SOURCE_PATH = './src/sources.txt'
+quotes = []
+with open(SOURCE_PATH, 'r') as f:
+    lines = f.readlines()
+
+for line in lines:
+    try:
+        _index = line.index(':')
+    except ValueError:
+        continue
+    author = line[:_index].strip()
+    quote = line[(_index + 1):].strip()
+    quotes.append((author, quote))
+try:
+    with open(GREAT_LIST_PATH, 'x') as f:
+        pass
+except FileExistsError:
+    pass
+
+with open(GREAT_LIST_PATH, 'r+') as f:
+    f_content = f.read().split()
+    great_list = list(range(0, 12))
+    try:
+        if len(f_content) > 0:
+            great_list = [int(i.strip()) for i in f_content]
+        today_random_quote_to_show = rd.choice(great_list)
+        great_list.remove(today_random_quote_to_show)
+    except ValueError:
+        today_random_quote_to_show = rd.randint(0, len(quotes) - 1)
+    f.seek(0)
+    f.truncate(0)
+    f.write(' '.join(str(i) for i in great_list))
+    f.flush()
+
+md_file_content = f'''
+    <p align=center>
+        <i>{quotes[today_random_quote_to_show][1]}<br>
+            --{quotes[today_random_quote_to_show][0]}--</i>
+    </p>
+'''
+with open("README.md", 'w') as f:
+    f.write(md_file_content)
+    f.flush()
+
